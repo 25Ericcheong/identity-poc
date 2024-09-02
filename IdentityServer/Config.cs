a1091ad90f64eef30a60f00341aa729f45ae57ec
25Ericcheong <ericcheongkaikit@gmail.com>
2024-09-03 07:43:47 +0800

    console able to obtain access token from auth server

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -12,9 +12,24 @@ public static class Config
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
-            { };
+            { new ApiScope(name: "delivery", displayName: "Delivery Name") };
 
     public static IEnumerable<Client> Clients =>
         new Client[] 
-            { };
+            { new Client
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "delivery" }
+            } };
 }
