5fa8ca8eb48dab7d33e884f905a3c39b3a9841fa
25Ericcheong <ericcheongkaikit@gmail.com>
2024-10-03 07:54:44 +0800

    chore: Full reset for code provided from vue starter pack

--- MvcApp/client/vite.config.ts
+++ MvcApp/client/vite.config.ts
@@ -7,6 +7,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  build: {
+    outDir: '../dist/front-end',
+    emptyOutDir: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
